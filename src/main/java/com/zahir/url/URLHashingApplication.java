226dcd60432e8d9e7d3b1d7992b2fc5a3c6316b6
zmandsaurwal <zmandsaurwal@opentext.com>
2022-06-16 13:10:42 -0400

    Initial Check-in

---
+++ src/main/java/com/zahir/url/URLHashingApplication.java
@@ -0,0 +1,12 @@
+package com.zahir.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class URLHashingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(URLHashingApplication.class, args);
+    }
+}
